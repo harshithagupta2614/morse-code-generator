ef1d0ce14984c159b79c207af01df1639d0cc040
Nehal Nevle <nehalnevle@gmail.com>
2021-04-29 19:07:55 +0530

    Add files via upload

---
+++ Morse-code.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Morse-code.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Morse-code.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=morse-code.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/morse-code.x.tar
